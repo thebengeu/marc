eda080bad35464fd7ff5799ba21274aecad40256
Beng Hee Eu <beng@benghee.eu>
2013-09-17 21:05:39 +0800

    Throw codemirror in

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,17 +12,28 @@ require.config({
                 'jquery'
             ],
             exports: 'Backbone'
+        },
+        codemirror: {
+            exports: 'CodeMirror'
+        },
+        codemirror_javascript: {
+            deps: ['codemirror']
         }
     },
     paths: {
         jquery: '../bower_components/jquery/jquery',
         backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/underscore/underscore'
+        underscore: '../bower_components/underscore/underscore',
+        codemirror: '../bower_components/codemirror/lib/codemirror',
+        codemirror_javascript: '../bower_components/codemirror/mode/javascript/javascript'
     }
 });
 
 require([
-    'backbone'
-], function (Backbone) {
+    'backbone',
+    'views/code'
+], function (Backbone, CodeView) {
     Backbone.history.start();
+
+    var codeView = (new CodeView({ el: '.codeView' })).render();
 });
