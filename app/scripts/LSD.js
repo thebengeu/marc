2cb9a5f9ac9e47ad1743078cdb8ded519263000e
Beng Hee Eu <beng@benghee.eu>
2013-09-20 17:34:40 +0800

    Add LSD stub, simply proxies localStorage for now

---
+++ app/scripts/LSD.js
@@ -0,0 +1,13 @@
+// LSD.js - localStorage, Distributed.
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals
+        root.LSD = factory();
+    }
+}(this, function () {
+    return localStorage;
+}));
