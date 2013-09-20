54749fc07f56deaa44c3fd2ad41b20e5f8f76068
Beng Hee Eu <beng@benghee.eu>
2013-09-20 20:16:55 +0800

    Add Scroll to Top button

--- app/scripts/views/code.js
+++ app/scripts/views/code.js
@@ -30,6 +30,22 @@ define([
                 lineWrapping: true,
                 readOnly: true
             });
+
+            // Add a "Scroll to Top" button to make the ugly space at the bottom
+            // less obvious, since it may be needed:
+            // "heightForcer is needed because behavior of elts with
+            // overflow: auto and padding is inconsistent across browsers".
+            $('.CodeMirror-sizer').next()
+                .width('100%')
+                .html('<div class="text-center">' +
+                    '<button type="button" class="btn btn-default btn-xs">' +
+                    '<span class="glyphicon glyphicon-arrow-up"></span>' +
+                    ' Scroll to Top ' +
+                    '<span class="glyphicon glyphicon-arrow-up"></span>' +
+                    '</button></div>')
+                .click(_.bind(function () {
+                    this.codeMirror.scrollIntoView(0);
+                }, this));
             return this;
         }
     });
