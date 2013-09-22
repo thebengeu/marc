c2887aee9ec364462d1283a1df1f99c524bf4c87
Kai Yao <yky234-github@yahoo.com.sg>
2013-09-22 23:44:10 +0800

    Added modal dialog to browse for a dropbox folder.

--- app/scripts/dropboxService.js
+++ app/scripts/dropboxService.js
@@ -10,16 +10,23 @@ define([
     client = new Dropbox.Client({ key: "fbor6xe2q47cmbf" });
     
     $('#add-from-dropbox').click(function() {
+        alert("add");
         authenticate();
     });
     
     var authenticate = function () {
+        var receiveUrl = location.href;
+        if (receiveUrl.split("#").length > 1){
+            receiveUrl = receiveUrl.split("#")[0];
+        }
+    
         client.authenticate(function(error, client) {
             if (error) {
                 // Replace with a call to your own error-handling code.
                 //
                 // Don't forget to return from the callback, so you don't execute the code
                 // that assumes everything went well.
+                alert (error);
                 return showError(error);
             }
 
@@ -27,6 +34,8 @@ define([
             //
             // The user authorized your app, and everything went well.
             // client is a Dropbox.Client instance that you can use to make API calls.
+            
+            $('#dialog-dropbox-browser').modal('show');
             
             browseFolder('/');
             console.log("hello");
@@ -37,7 +46,16 @@ define([
         client.readdir(path, function(error, entries, dirInfo, dirContentInfo) {
             if (error) {
                 return showError(error);  // Something went wrong.
-            }
+            }           
+            
+            $('#dialog-dropbox-browser .modal-body').html("");            
+            _.each(dirContentInfo, function(item){
+                if (item.isFolder){
+                    $('#dialog-dropbox-browser .modal-body').append("<a href='#'>"+item.name+"</a><br>");
+                }else{
+                    $('#dialog-dropbox-browser .modal-body').append(item.name+"<br>");
+                }
+            });
             
             console.log(entries);
             console.log(dirInfo);
