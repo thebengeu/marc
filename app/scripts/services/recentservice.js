3996761442041b67f9134c7dc2dd12512f25fa32
Benedict Liang <benedictljj@gmail.com>
2013-09-23 20:41:00 +0800

    Added recentservice.js.

---
+++ app/scripts/services/recentservice.js
@@ -0,0 +1,27 @@
+/*global define*/
+
+/**
+ * Similar to a stack.
+ */
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'LSD'
+], function ($, _, Backbone, LSD) {
+    var pushRoute = function(route) {
+
+    };
+
+    var popRoute = function() {
+
+    };
+
+    var peekRoute = function() {
+
+    };
+
+    var isEmpty = function() {
+
+    };
+});
