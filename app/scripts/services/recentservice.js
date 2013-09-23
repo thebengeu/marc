24c356e38443a1f93cb9a5548a7c666f2c30236c
Benedict Liang <benedictljj@gmail.com>
2013-09-23 20:53:50 +0800

    Implemented recent routes stack.

--- app/scripts/services/recentservice.js
+++ app/scripts/services/recentservice.js
@@ -9,19 +9,48 @@ define([
     'backbone',
     'LSD'
 ], function ($, _, Backbone, LSD) {
-    var pushRoute = function(route) {
 
+    var storageRouteKey = 'route';
+
+    var pushRoute = function(route) {
+        var routeStack = getRouteStack();
+
+        routeStack.push(route);
+
+        LSD.setItem(storageRouteKey, routeStack);
     };
 
     var popRoute = function() {
+        var routeStack = getRouteStack();
 
+        if (!isEmpty(routeStack)) {
+            routeStack.pop();
+        }
+
+        LSD.setItem(storageRouteKey, routeStack);
     };
 
     var peekRoute = function() {
+        var routeStack = getRouteStack();
 
+        if (!isEmpty(routeStack)) {
+            return routeStack[routeStack.length - 1];
+        }
+
+        return null
     };
 
-    var isEmpty = function() {
+    var isEmpty = function(stack) {
+        return stack.length == 0;
+    };
 
+    var getRouteStack = function() {
+        var routeStack = LSD.getItem('route');
+
+        if (routeStack) {
+            return routeStack;
+        }
+
+        return [];
     };
 });
