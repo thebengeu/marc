aa65d43f2f653a77b4ab4050fb6753fae5fe06e9
Jerome Cheng <jcheng31@gmail.com>
2013-09-24 11:41:33 +0800

    Add more languages to extToMode.

--- app/scripts/extToMode.js
+++ app/scripts/extToMode.js
@@ -2,9 +2,26 @@
 
 define({
     css: 'css',
+    c: 'clike',
+    cs: 'clike',
+    clj: 'clojure',
+    coffee: 'coffeescript',
+    d: 'd',
+    erb: 'ruby',
+    erl: 'erlang',
+    groovy: 'groovy',
+    jade: 'jade',
+    java: 'clike',
     js: 'javascript',
     json: { name: 'javascript', json: true },
+    lisp: 'commonlisp',
+    hs: 'haskell',
     htm: 'htmlmixed',
     html: 'htmlmixed',
-    md: 'markdown'
+    md: 'markdown',
+    php: 'php',
+    scala: 'clike',
+    ts: 'javascript',
+    xaml: 'xml',
+    xml: 'xml'
 });
