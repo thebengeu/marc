dbc5364ea52e93f0dae2fa8e110698a9884e11ed
Beng Hee Eu <beng@benghee.eu>
2013-09-25 10:45:02 +0800

    Add clearing of local storage if schema has breaking changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -85,6 +85,8 @@ require.config({
     }
 });
 
+var SCHEMA_VERSION = '0.0.0';
+
 require([
     'underscore',
     'backbone',
@@ -98,13 +100,20 @@ require([
     'fastclick',
     'services/FileLoader',
     'services/file',
+    'LSD',
     'bootstrap',
     'jqTree',
     'dropbox',
     'bootstrap-switch',
     'views/settingspane'
 ], function (_, Backbone, Sidebar, FileList, ApplicationRouter, ServicesRouter,
-    File, Snap, enquire, FastClick, FileLoader, FileService, SettingsPaneView) {
+    File, Snap, enquire, FastClick, FileLoader, FileService, LSD, SettingsPaneView) {
+    // Clear local storage if schema has breaking changes.
+    if (LSD.getItem('v') !== SCHEMA_VERSION) {
+        LSD.clear();
+        LSD.setItem('v', SCHEMA_VERSION);
+    }
+
     new ApplicationRouter();
     new ServicesRouter();
     Backbone.history.start();
