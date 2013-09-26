10a73de2b2d489719fe7637c110f3f6dfae8250c
Kai Yao <yky234-github@yahoo.com.sg>
2013-09-26 14:43:29 +0800

    Changed "add-from-dropbox" link to use root-relative path. Improved Dropbox error messages.

--- app/scripts/services/dropbox.js
+++ app/scripts/services/dropbox.js
@@ -25,8 +25,47 @@ define([
         $('#dialog-dropbox-browser').modal('hide');
     });
 
-    var showError = function (error) {
-        alert(error);
+    var showError = function(error) {
+        switch (error.status) {
+        case Dropbox.ApiError.INVALID_TOKEN:
+            // If you're using dropbox.js, the only cause behind this error is that
+            // the user token expired.
+            // Get the user through the authentication flow again.
+            alert('Sorry, your Dropbox session has expired. Please try again.');
+            break;
+
+        case Dropbox.ApiError.NOT_FOUND:
+            // The file or folder you tried to access is not in the user's Dropbox.
+            // Handling this error is specific to your application.
+            break;
+
+        case Dropbox.ApiError.OVER_QUOTA:
+            // The user is over their Dropbox quota.
+            // Tell them their Dropbox is full. Refreshing the page won't help.
+            break;
+
+        case Dropbox.ApiError.RATE_LIMITED:
+            // Too many API requests. Tell the user to try again later.
+            // Long-term, optimize your code to use fewer API calls.
+            alert('Sorry, we is unable to service your request. Please try again later.');
+            break;
+
+        case Dropbox.ApiError.NETWORK_ERROR:
+            // An error occurred at the XMLHttpRequest layer.
+            // Most likely, the user's network connection is down.
+            // API calls will not succeed until the user gets back online.
+            alert('A network error has occured. Please check your Internet connection and try again.');
+            break;
+
+        case Dropbox.ApiError.INVALID_PARAM:
+        case Dropbox.ApiError.OAUTH_ERROR:
+        case Dropbox.ApiError.INVALID_METHOD:
+        default:
+            // Caused by a bug in dropbox.js, in your application, or in Dropbox.
+            // Tell the user an error occurred, ask them to refresh the page.
+            alert('Sorry, it appears that an error has occured. Please try again.\n\nIf you did not grant m(arc) permission to access your Dropbox files, please try again and grant the required permissions.');
+
+        }
     };
 
     var authenticate = function (callback) {
@@ -173,12 +212,14 @@ define([
         authenticate: authenticate,
         showModal: showModal,
         get: function (path, callback) {
-            console.log(path);
-            client.readFile(path, function (error, data) {
-                if (error) {
-                    return showError(error);  // Something went wrong.
-                }
-                callback(data);  // data has the file's contents
+            authenticate(function(){
+                console.log(path);
+                client.readFile(path, function (error, data) {
+                    if (error) {
+                        return showError(error);  // Something went wrong.
+                    }
+                    callback(data);  // data has the file's contents
+                });
             });
         },
         updateFile: function(file, callback) {
